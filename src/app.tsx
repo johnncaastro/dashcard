611a27f34ce722eed68c9ee6bc563ab709edbee8
Jonathan Alves <you@example.com>
2024-03-29 23:09:28 -0300

    style: create session activity

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import { Header } from './components/header'
+import Chart, { Props } from 'react-apexcharts'
 import CardImg from './assets/images/credit-card.png'
 import {
   ShoppingBag,
@@ -13,11 +14,92 @@ import {
   TransactionCardText,
 } from './components/transaction-card'
 
+const chart: Props = {
+  options: {
+    colors: ['#8743d8'],
+    chart: {
+      id: 'basic-bar',
+      height: '100%',
+      fontFamily: 'Poppins, sans-serif',
+      toolbar: {
+        show: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    grid: {
+      show: false,
+    },
+    annotations: {
+      xaxis: [
+        {
+          x: 'Tu',
+          borderWidth: 0,
+          label: {
+            text: '$145',
+            borderColor: '#1c1c1c',
+            borderRadius: 3,
+            orientation: 'horizontal',
+            offsetY: 30,
+            style: {
+              color: '#fff',
+              background: '#1c1c1c',
+              padding: {
+                left: 8,
+                right: 8,
+                top: 2,
+                bottom: 2,
+              },
+            },
+          },
+        },
+      ],
+      points: [
+        {
+          x: 'Tu',
+          y: 145,
+          marker: {
+            size: 4,
+            fillColor: '#1c1c1c',
+          },
+        },
+      ],
+    },
+    xaxis: {
+      categories: ['', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', ''],
+      labels: {
+        style: {
+          colors: '#b7b7b7',
+          fontWeight: 500,
+        },
+      },
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+    },
+    yaxis: {
+      show: false,
+      min: 0,
+      max: 220,
+    },
+  },
+  series: [
+    {
+      name: 'Expense',
+      data: [160, 120, 145, 160, 180, 200, 220, 195, 160],
+    },
+  ],
+}
+
 export function App() {
   return (
     <>
       <Header />
-      <div className="min-h-9/10 mt-8 grid grid-cols-9 grid-rows-6 gap-8">
+      <div className="mt-8 grid min-h-9/10 grid-cols-9 grid-rows-6 gap-8">
         <div className="col-span-6 row-span-3 flex items-start justify-between rounded-3xl bg-white-100 px-6 py-4">
           <div className="flex-1">
             <h2 className="text-xl font-semibold">My card</h2>
@@ -75,8 +157,9 @@ export function App() {
         <div className="col-span-3 row-span-6 rounded-3xl bg-white-100 px-6 py-4">
           <h2>Expense Control</h2>
         </div>
-        <div className="col-span-3 row-span-3 rounded-3xl bg-white-100 px-6 py-4">
-          <h2 className="text-xl font-semibold">Activity</h2>
+        <div className="col-span-3 row-span-3 rounded-3xl bg-white-100">
+          <h2 className="px-6 pt-4 text-xl font-semibold">Activity</h2>
+          <Chart options={chart.options} series={chart.series} type="area" />
         </div>
         <div className="col-span-3 row-span-3 rounded-3xl bg-white-100 px-6 py-4">
           <h2 className="text-xl font-semibold">Payments</h2>
